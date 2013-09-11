82b24b474218c202a24bf0fbcfd7d9277a462408
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-11 16:49:03 -0400

    Made new routes for User

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rainforest::Application.routes.draw do
     resources :reviews, :except => [:index]
   end
 
-  resources :users, :only => [:new, :create]
+  resources :users, :except => [:index]
   resources :sessions, :only => [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
